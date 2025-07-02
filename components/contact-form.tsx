21d354524ed90683e548a5a1b96576dc9d1ca126
Ondřej Topínka <topetopinka7@seznam.cz>
2025-07-03 00:35:53 +0200

    idk

--- components/contact-form.tsx
+++ components/contact-form.tsx
@@ -86,7 +86,7 @@ export function ContactForm() {
 
     return (
         <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+            <form onSubmit={form.handleSubmit(onSubmit)} className="z-10 space-y-6 bg-background">
                 {/* Full Name Field */}
                 <FormField
                     control={form.control}
